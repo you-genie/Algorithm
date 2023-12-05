60a4b077e7245e288f5ef130f4171e8799eba763
Genne Chung <parvaavis867@gmail.com>
2023-12-05 10:41:23 +0900

    [level 2] Title: 요격 시스템, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -0,0 +1,16 @@
+def solution(targets):
+    sorted_targets = sorted(targets, key=lambda x: x[0])
+    o_s, o_e = -1, -1
+    answer = 0
+    
+    for s, e in sorted_targets:
+        if o_e <= s:
+            # not overlap, 미사일 추가
+            answer += 1
+            o_s, o_e = s, e
+        else:
+            # overlap, 미사일 구간만 변경
+            o_s = max(o_s, s)
+            o_e = min(o_e, e)
+    
+    return answer
