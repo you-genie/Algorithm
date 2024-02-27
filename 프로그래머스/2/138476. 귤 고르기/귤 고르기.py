0f652450f8647481fe7f96c330e6b8ed4428293d
Genne Chung <parvaavis867@gmail.com>
2024-02-28 08:09:48 +0900

    [level 2] Title: 귤 고르기, Time: 31.69 ms, Memory: 21.8 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,12 +1,12 @@
-from collections import Counter
+from collections import Counter, deque
 
 def solution(k, tangerine):
     answer = 0
     
     counter = Counter(tangerine)
-    sizes = sorted([(size, n) for size, n in counter.items()], key=lambda x: x[1], reverse=True)
+    sizes = deque(sorted([(size, n) for size, n in counter.items()], key=lambda x: x[1], reverse=True))
     
     while k > 0:
-        k -= sizes.pop(0)[1]
+        k -= sizes.popleft()[1]
         answer += 1
-    return answer
+    return answer 
